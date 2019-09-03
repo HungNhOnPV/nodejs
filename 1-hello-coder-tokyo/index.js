40b42aeb9d801945e808774de63db1725595e3dd
HungNhOnPV <anhhungx9propro@gmail.com>
2019-09-03 17:03:57 +0700

    express router

--- 1-hello-coder-tokyo/index.js
+++ 1-hello-coder-tokyo/index.js
@@ -1,20 +1,11 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-var low = require('lowdb');
-var shortid = require('shortid');
 
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
-
-db =low(adapter);
-
-db.defaults({
-	users: []
-}).write();
+var userRoute = require('./routes/user.routes');
 
 var port = 3000;
 
+var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -27,43 +18,8 @@ app.get('/', function(req, res) {
 	});
 });
 
-app.get('/users', function(req, res) {
-	res.render('users/index', {
-		users: db.get('users').value()
-	});
-});
+app.use('/users', userRoute);
 
-app.get('/users/search', function(req, res) {
-	var q = req.query.q;
-	var matchedUsers = users.filter(function(user) {
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-	});
-	
-	res.render('users/index', {
-		users: matchedUsers
-	});
-});
-
-app.get('/users/create', function(req, res) {
-	res.render('users/create');
-});
-
-app.get('/users/:id', function(req, res) {
-	var id = req.params.id;
-
-	var user = db.get('users').find({ id: id }).value();
-
-	res.render('users/view', {
-		user: user
-	});
-});
-
-app.post('/users/create', function(req, res) {
-	req.body.id = shortid.generate();
-	db.get('users').push(req.body).write();
-	res.redirect('/users');
-})
-
-app.listen(3000, function() {
+app.listen(port, function() {
 	console.log('Server listening on port ' + port);
 });
