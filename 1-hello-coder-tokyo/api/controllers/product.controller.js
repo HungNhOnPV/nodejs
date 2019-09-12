1e97bcd73d548f67211489c927315bdd7037f0a5
HungNhOnPV <anhhungx9propro@gmail.com>
2019-09-12 16:32:16 +0700

    REST API

--- 1-hello-coder-tokyo/api/controllers/product.controller.js
+++ 1-hello-coder-tokyo/api/controllers/product.controller.js
@@ -2,25 +2,11 @@ var Product = require('../../models/product.model');
 // var db = require('../db');
 
 module.exports.index = async function(req, res) {
-  // var page = parseInt(req.query.page) || 1; // n
-  // var perPage = 8; // x
-
-  // var start = (page - 1) * perPage;
-  // var end = page * perPage;
-
-  // var drop = (page - 1) * perPage;
-
-  // res.render('products/index', {
-  //   //products: db.get('products').value().slice(start, end)
-  //   products: db.get('products').drop(drop).take(perPage).value()
-  // });
-
-  // Product.find().then(function(products) {
-  //   res.render('products/index', {
-  //     products: products
-  //   });
-  // });
-
-  var products = await Product.find();
+	var products = await Product.find();
 	res.json(products);
+};
+
+module.exports.create = async function(req, res) {
+	var product = await Product.create(req.body);
+	res.json(product);
 };
